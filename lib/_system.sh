9cec69cc794483c0fe12acbd47b76c6e49913403
dgstx <dgsomacal@gmail.com>
2023-06-22 20:56:41 -0300

    save

--- lib/_system.sh
+++ lib/_system.sh
@@ -460,7 +460,7 @@ phpmyadmin_install() {
   sudo apt install -y gettext php7.4-gettext
 
   # Lógica para instalação do phpMyAdmin no servidor
-  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin php-mbstring
+  apt-get install -y phpmyadmin php-mbstring
 
   #limpa nginx
   sudo rm -f /etc/nginx/sites-available/${sub_phpmy}
