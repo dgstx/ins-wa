c77742ea5ee03f91d371ccd927dd83bc761ccf68
dgstx <dgsomacal@gmail.com>
2023-06-22 13:39:17 -0300

    acrecimo de logica

--- lib/_frontend.sh
+++ lib/_frontend.sh
@@ -103,6 +103,7 @@ sudo su - deploy << EOF
 const express = require("express");
 const path = require("path");
 const app = express();
+require('dotenv').config();
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
